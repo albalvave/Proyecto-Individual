1ac653d58d26171a4475512562da7b6b225caed6
Alba Alvarez <albaaven@gmail.com>
2023-10-14 18:26:44 +0200

    Enunciados provisionales

---
+++ Enunciados.R
@@ -0,0 +1,41 @@
+#1.Problema 1:
+  
+#Somos un inversor que debe tomar decisiones sobre cuatro posibles inversiones, 
+#representadas como decisiones (d1, d2, d3, d4), con cuatro eventos (E1, E2, E3, E4). 
+#Aplicar los criterios de decision bajo incertidumbre tanto en el caso favorable (beneficios), 
+#como en el caso desfavorable (costos).
+
+
+    | E1 | E2 | E3 | E4 |
+  
+  d1| 30 | -5 | 20 | 28 |
+  d2| 20 | 22 | 25 | -8 |
+  d3|  5 | 18 | 12 | 14 |
+  d4| 25 | 24 | 0  | 26 |
+ 
+       
+  
+  
+#2.Problema 2: 
+  
+  
+#Lyre quiere unirse a un gimnasio y ha investigado tres opciones de membresía.Cada membresía tiene sus propias condiciones y costos asociados:
+  
+  
+#Membresía "Basic Fit": Requiere un pago mensual de 20 euros, pero no cubre todas las 
+#instalaciones y clases de gimnasio hasta después de seis meses. Durante los primeros seis meses, solo tiene acceso limitado.
+
+#Membresía "Fitness Plus": Ofrece acceso completo a todas las instalaciones y clases
+#desde el primer día, pero con una cuota mensual de 40 euros.
+
+#Membresía "Gym Pro": Requiere un pago mensual de 30 euros y cubre todas las instalaciones
+#y clases, excepto las clases de natación.
+
+#Lyre planea mantener su membresía de gimnasio durante un año, pero no está segura si usará
+#todas las instalaciones y clases desde el principio debido a su apretado horario en la universidad o si esperará 
+#los primeros seis meses antes de hacerlo.
+
+#El costo de acceso a las instalaciones y clases de gimnasio es de 15 euros por mes. Si Lyre
+#quiere evaluar el costo total durante el primer año, ¿qué membresía de gimnasio le recomendarías?
+  
+  
